55820ca5404faa9c54952854713bd43e11d2ddc9
Leandro <gonzalezanriquez@gmail.com>
2024-07-23 13:16:45 -0300

    WELCOME

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,75 +38,70 @@
             </div>
         
         <!-- TARJETAS PRESENTACIONES -->
-<!-- component -->
-    <!-- ====== Cards Section Start -->
     <section class="bg-gray-2 dark:bg-dark pb-10  ">
         <div class="container mx-auto">
             <div class="flex flex-wrap -mx-2">
                 
                 <div class="w-full px-4 md:w-1/2 xl:w-1/3 ">
                     <div class="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3 shadow-md">
-                        <img src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-01.jpg"
+                        <img src="{{ asset('img/1.jpg')}}"
                             alt="image" class="w-full" />
                         <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
-                            <h3>
-                                <a href="javascript:void(0)" class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
-                                </a>
-                            </h3>
+                            <h2  class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                                EFICAZ
+                            </h2>
                             <p class="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                                Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                                elit. Lorem consectetur adipiscing elit.
+                                Creemos en una solución eficaz e integral, que logre abordar tanto las necesidades del alumno, del docente así como también y la dirección académica todo en un mismo espacio
                             </p>
-                            <a href="javascript:void(0)" class="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6">
-                                View Details
-                            </a>
                         </div>
                     </div>
                 </div>
 
-                <div class="w-full px-4 md:w-1/2 xl:w-1/3">
-                    <div
-                        class="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
-                        <img src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-02.jpg"
-                            alt="image" class="w-full" />
+                
+                <div class="w-full px-4 md:w-1/2 xl:w-1/3 ">
+                    <div class="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3 shadow-md">
+                        <img src="{{ asset('img/2.jpg')}}" alt="image" class="w-full" />
                         <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
-                            <h3>
-                                <a href="javascript:void(0)"
-                                    class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
-                                    The ultimate UX and UI guide to card design
-                                </a>
-                            </h3>
-                            <p class="text-base leading-relaxed text-body-color mb-7">
-                                Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                                elit. Lorem consectetur adipiscing elit.
-                            </p>
-                            <a href="javascript:void(0)"
-                                class="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6">
-                                View Details
-                            </a>
+                            <h2  class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                                ADAPTABLE
+                            </h2>
+                            <p class="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
+                                Armado de estructuras inteligentes y dinamicas para una organización clara y eficiente.                            </p>
                         </div>
                     </div>
                 </div>
+
+
+                <div class="w-full px-4 md:w-1/2 xl:w-1/3 ">
+                    <div class="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3 shadow-md">
+                        <img src="{{ asset('img/1.jpg')}}"
+                            alt="image" class="w-full" />
+                        <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                            <h2  class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                                EFICAZ
+                            </h2>
+                            <p class="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
+                                Creemos en una solución eficaz e integral, que logre abordar tanto las necesidades del alumno, del docente así como también y la dirección académica todo en un mismo espacio
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+
                 <div class="w-full px-4 md:w-1/2 xl:w-1/3">
                     <div
                         class="mb-10 overflow-hidden duration-300 bg-white rounded-lg dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
-                        <img src="https://cdn.tailgrids.com/2.0/image/application/images/cards/card-01/image-03.jpg"
+                        <img src="{{ asset('img/3.jpg')}}"
                             alt="image" class="w-full" />
                         <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
-                            <h3>
-                                <a href="javascript:void(0)"
-                                    class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
-                                    Creative Card Component designs graphic elements
-                                </a>
-                            </h3>
+                            <h2  class="text-dark dark:text-white hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                                VIRTUAL
+                            </h2>
                             <p class="text-base leading-relaxed text-body-color mb-7">
-                                Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                                elit. Lorem consectetur adipiscing elit.
+                                Digitalizar los procesos administrativos escolares. Establecer un punto de comunicación digital Alumno - Institución - docente. Brindar información académica general.
                             </p>
-                            <a href="javascript:void(0)"
-                                class="inline-block py-2 text-base font-medium transition border rounded-full text-body-color hover:border-primary hover:bg-primary border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6">
-                                View Details
-                            </a>
+                           
                         </div>
                     </div>
                 </div>
