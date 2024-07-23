988e10e2f2be21463f69c279cd21f67358bb0960
LEANDRO ANRIQUEZ <99001904+gonzalezanriquez@users.noreply.github.com>
2024-07-23 13:29:53 -0300

    correcciones js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,6 @@ import feather from 'feather-icons';
 import 'flowbite';
 import Swal from 'sweetalert2';
 
-const Swal = require('sweetalert2');
-
-import Swal from 'sweetalert2/dist/sweetalert2.js';
-import 'sweetalert2/src/sweetalert2.scss';
-
 window.Alpine = Alpine;
 
 Alpine.start();
